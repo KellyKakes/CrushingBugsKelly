98d67f1767cca222b06cc894ebb910ccd982326a
Kelly Kakekagumick <91221305+KellyKakes@users.noreply.github.com>
2024-07-03 16:24:45 -0400

    The whole thing had a confliction
    I don't know what happened, the whole thing had a confliction that wasn't able to be merged. So I had to fix them. Mostly deleted a ton of notes and extra stuff that didn't need to be there. I'm unsure, the code worked, like the site was doing great, it just was being weird.

--- js/main.js
+++ js/main.js
@@ -8,29 +8,10 @@ const dropZones = document.querySelectorAll(".drop-zone");
 let draggedPiece;
 
 //functions
-
 function changeBGImage(event) {
-<<<<<<< Updated upstream
-    //console.log("changeBGImage called");
-    //method 1
-    //console.log(this.id);
-    //background-image: url('../images/backGround0.jpg');
-    //puzzleBoard.style.backgroundImage = `url('../images/backGround${this.id}.jpg')`;
-
-    //Method 2
-    console.log(event.currentTarget.id);
-    puzzleBoard.style.backgroundImage = `url('../images/backGround${event.currentTarget.id}.jpg')`;
-
-=======
-    //fix goes here
     puzzlePieces.forEach(piece => puzzlePieceDiv.appendChild(piece));
     console.log(event.currentTarget.id);
-    //puzzlePieces.forEach(piece => {puzzlePieceDiv appendchild(piece)})
-    //Wait I just changed the {} these things and it worked?
-
-    //Method 2
     puzzleBoard.style.backgroundImage = `url('./images/backGround${event.currentTarget.id}.jpg')`;
->>>>>>> Stashed changes
 }
 
 function handleStartDrag() {
@@ -44,28 +25,16 @@ function handleOver(e) {
 }
 
 function handleDrop() {
-<<<<<<< Updated upstream
-=======
-    //this is where the fix will go
-
-    //going to use an if statement
-
-    //One piece can no longer be in the same Container Drop Zone. but it doesn't reset into default position.
     if(this.children.length >=1) {
      return;
     }
->>>>>>> Stashed changes
     this.appendChild(draggedPiece);
-    console.log(event.currentTarget.id);
 }
 
 console.log(puzzlePieces);
+
 //eventListeners
-
 theButtons.forEach(button => button.addEventListener("click", changeBGImage));
-
 puzzlePieces.forEach(piece => piece.addEventListener("dragstart", handleStartDrag));
-
 dropZones.forEach(zone => zone.addEventListener("dragover", handleOver));
-
 dropZones.forEach(zone => zone.addEventListener("drop", handleDrop));
